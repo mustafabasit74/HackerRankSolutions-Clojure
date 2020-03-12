68ec48f6c70a492616c4c9c69e98aa10ac428343
Mustafa Basit <mailtobasit74@gmail.com>
2020-03-12 20:34:09 +0530

    modified - filter position and list replication program

--- src/hackerrank/Filter Positions in a List.clj
+++ src/hackerrank/Filter Positions in a List.clj
@@ -1,4 +1,18 @@
-(defn filter-list
-    [collection]
-    (take-nth 2 (rest collection)) )
-      
+(defn do-filter
+    ([given-list]
+        (do-filter given-list []  1 ) )
+    
+    ([given-list filtered-list counter]
+        (if (empty? given-list)
+              filtered-list
+              (if (odd? counter)
+                    (recur (vec (rest given-list)) filtered-list (inc counter) )
+                    (recur (vec (rest given-list)) (conj filtered-list (first given-list))  (inc counter)) ) ) ) )
+
+;-----------------------------------------------------------
+
+;using inbuilt functions
+
+;    (defn do-filter 
+;      [collection]
+;      (take-nth 2 (rest collection)) )
--- src/hackerrank/List Replication.clj
+++ src/hackerrank/List Replication.clj
@@ -1,12 +1,12 @@
 (defn do-replicate
-    ([repeat-n-times collection replicated-collection max-count]  
-    (if (empty? collection)
-          replicated-collection
-          (do 
+      ([repeat-n-times collection replicated-collection max-count]  
+      (if (empty? collection)
+            replicated-collection
             (if (= max-count repeat-n-times)
-                  (recur repeat-n-times (vec (rest collection) )  (conj replicated-collection (first collection) ) 1 )
-                  (recur repeat-n-times collection (conj replicated-collection (first collection) ) (inc max-count) ) ) ) ) )
-    ([repeat-n-times collection]
+                        (recur repeat-n-times (vec (rest collection) )  (conj replicated-collection (first collection) ) 1 )
+                        (recur repeat-n-times collection (conj replicated-collection (first collection) ) (inc max-count) ) ) ) ) 
+    
+      ([repeat-n-times collection]
           (do-replicate repeat-n-times collection [] 1) ) )
 
   ;(do-replicate 3 [1 2 3 4 5])
