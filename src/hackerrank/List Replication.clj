f0a96e40308af8e1f9800f5c14f6732bef4150f9
Mustafa Basit <mailtobasit74@gmail.com>
2020-03-12 19:16:30 +0530

    modified list replication program

--- src/hackerrank/List Replication.clj
+++ src/hackerrank/List Replication.clj
@@ -1,7 +1,20 @@
-(defn replicate-list 
-    [repeat-n-times collection]
-    (flatten (map #(repeat repeat-n-times %) collection) ) )
-
-
-;(def number-list [3 1 2 3 4])
-;(replicate-list (first number-list) (rest number-list))
+(defn do-replicate
+    ([repeat-n-times collection replicated-collection max-count]  
+    (if (empty? collection)
+          replicated-collection
+          (do 
+            (if (= max-count repeat-n-times)
+                  (recur repeat-n-times (vec (rest collection) )  (conj replicated-collection (first collection) ) 1 )
+                  (recur repeat-n-times collection (conj replicated-collection (first collection) ) (inc max-count) ) ) ) ) ) 
+    ([repeat-n-times collection]
+          (do-replicate repeat-n-times collection [] 1) ) )
+  
+  ;(do-replicate 3 [1 2 3 4 5])
+  
+  ;using inbuilt functions
+  
+  ;(defn do-replicate 
+  ;   [repeat-n-times collection]
+  ;  (flatten (map #(repeat repeat-n-times %) collection) ) )
+  
+  ;(do-replicate 3 [1 2 3 4 5])
