a613a4a1f3a760c237936e3a98393e3809a014d8
Mustafa Basit <mailtobasit74@gmail.com>
2020-03-12 22:10:14 +0530

    modified filter array and filter positions program

--- src/hackerrank/Filter Array.clj
+++ src/hackerrank/Filter Array.clj
@@ -1,3 +1,26 @@
-(defn filter-collection
-    [delimiter collection]
-    (filter #(< % delimiter) collection)   )
+(defn do-filter
+  ([delimiter given-collection] 
+     (do-filter delimiter given-collection [] ) )
+
+  ([delimiter given-collection filtered-collection]
+    (if (empty? given-collection)
+          filtered-collection 
+          (if (< (first given-collection) delimiter) 
+                (recur delimiter (vec (rest given-collection)) (conj filtered-collection (first given-collection)) )
+                (recur delimiter (vec (rest given-collection)) filtered-collection ) ) ) ) )
+
+;(do-filter 50 [14 83 20 47 55 65 11 4 92 31 50 27 13])
+;(do-filter 50 [])
+
+;------------------------------------------------------------------------------
+
+;using inbuilt functions
+
+
+;(defn do-filter
+;    [delimiter given-collection]
+;     (vec (filter #(< % delimiter) given-collection)) )
+;;     (vec .... ----------becaues "(do-filter 50 [] )" returns empty list "()" instead of empty vector "[]" 
+
+;(do-filter 50 [14 83 20 47 55 65 11 4 92 31 50 27 13])
+;(do-filter 50 [])
--- src/hackerrank/Filter Positions in a List.clj
+++ src/hackerrank/Filter Positions in a List.clj
@@ -9,6 +9,9 @@
                     (recur (vec (rest given-list)) filtered-list (inc counter) )
                     (recur (vec (rest given-list)) (conj filtered-list (first given-list))  (inc counter)) ) ) ) )
 
+;(do-filter [1 2 3 4 5 6 7 8 9 10])
+;(do-filter [1])
+
 ;-----------------------------------------------------------
 
 ;using inbuilt functions
